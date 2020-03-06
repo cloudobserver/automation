76071278174585a0f91704eb3b97252ac32a6c2c
Andrew Bartson <andrewbartson@Andrews-MacBook-Pro.local>
2020-03-05 18:49:12 -0700

    add page object for yoodlize.js - try again :)

---
+++ pageObjects/yoodlizePgObj.js
@@ -0,0 +1,53 @@
+module.exports = {
+  url: 'https://alpha.yoodlize.com/',
+  elements: {
+    logoHomeBtn: 'a.navbar-brand',
+    searchFieldHome: {
+      selector: '//div[@class="form-group"]/input',
+      locateStrategy: 'xpath'
+    },
+    searchButton: {
+      selector: '//div[@class="form-group"]/input/following-sibling::button',
+      locateStrategy: 'xpath'
+    },
+    blueKeywordFilter: {
+      selector: '//div[@class="sc-jKVCRD jSqgxr"]',
+      locateStrategy: 'xpath'
+    },
+    searchFieldResults: '#keyword-search-input',
+    itemTitleResults: '#card-title',
+    imageNavRight: 'i.fa-angle-right',
+    imageNavLeft: 'i.fa-angle-left',
+    itemPriceResults: 'div._2J6OR._246En._3hOq8',
+    imageLink: 'div._1WAWo',
+    itemTitleDetails: 'div.hxTVNb.fptSCa',
+    itemCityDetails: 'div.lhtIrb.jmxqrS',
+    itemRentalRules: 'ul.sc-fYiAbW.fhICTT',
+    description: {
+      selector: '//div[contains(text(), "Description")]/following-sibling::div',
+      locateStrategy: 'xpath'
+    },
+    itemOwner: {
+      selector: '//a[@href="/users/show/222"]',
+      locateStrategy: 'xpath'
+    }
+
+
+    // de: {
+    //   selector: '',
+    //   locateStrategy: 'xpath'
+    // },
+    // ue: {
+    //   selector: '',
+    //   locateStrategy: 'xpath'
+    // }
+
+    // bInput: 'input[name="phoneEntry"]',
+    // cInput: 'input[name="titleEntry"]',
+    // cInput: 'input[name="datInput"]',
+    // submitBtn: '#saveBtn',
+    // validHeader: '#validHeader',
+    // body: 'body'
+  },
+  commands: [{}]
+}
